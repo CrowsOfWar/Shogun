8f8d1d5ae658cb99786f8cb618af6d94ba32165f
Crows of War <ofwarcrows@gmail.com>
2015-09-20 19:00:22 -0500

    Social classes! Will be CHANGED LATER

---
+++ src/main/java/crowsofwar/shogun/common/society/ShogunClass.java
@@ -0,0 +1,37 @@
+package crowsofwar.shogun.common.society;
+
+/**
+ * The different social classes in Feudal Japan. Some you
+ * cannot achieve at first.
+ * 
+ * @author CrowsOfWar
+ */
+public enum ShogunClass {
+	
+	BARBARIAN(true, -300),
+	BARBARIAN_LONGER(false, -50),
+	PEASANT(false, 100),
+	SAMURAI(false, 300),
+	RONIN(true, 50),
+	DAIMYO(false, 400),
+	ROYALTY(true, 500),
+	SHOGUN(false, 700),
+	EMPEROR(false, 800);
+	
+	private final boolean startingClass;
+	private final int defaultHonor;
+	
+	private ShogunClass(boolean startingClass, int defaultHonor) {
+		this.startingClass = startingClass;
+		this.defaultHonor = defaultHonor;
+	}
+	
+	public boolean canStartWith() {
+		return startingClass;
+	}
+	
+	public int getDefaultHonor() {
+		return defaultHonor;
+	}
+	
+}
