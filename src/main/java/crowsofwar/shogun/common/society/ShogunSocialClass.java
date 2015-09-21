db33b73d609c9587c69ac3631f2f3cf8b1f6bd09
Crows of War <ofwarcrows@gmail.com>
2015-09-20 19:08:29 -0500

    get social class by ID method

--- src/main/java/crowsofwar/shogun/common/society/ShogunSocialClass.java
+++ src/main/java/crowsofwar/shogun/common/society/ShogunSocialClass.java
@@ -1,5 +1,8 @@
 package crowsofwar.shogun.common.society;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * The different social classes in Feudal Japan. Some you
  * cannot achieve at first.
@@ -8,6 +11,7 @@ package crowsofwar.shogun.common.society;
  */
 public enum ShogunSocialClass {
 	
+	ERROR(false, 0),
 	BARBARIAN(true, -300),
 	BARBARIAN_LONGER(false, -50),
 	PEASANT(false, 100),
@@ -18,6 +22,7 @@ public enum ShogunSocialClass {
 	SHOGUN(false, 700),
 	EMPEROR(false, 800);
 	
+	private static final Map<Integer, ShogunSocialClass> classByID;
 	private final boolean startingClass;
 	private final int defaultHonor;
 	
@@ -26,12 +31,25 @@ public enum ShogunSocialClass {
 		this.defaultHonor = defaultHonor;
 	}
 	
+	static {
+		classByID = new HashMap<Integer, ShogunSocialClass>();
+		for (ShogunSocialClass socialClass : values()) classByID.put(socialClass.id(), socialClass);
+	}
+	
 	public boolean canStartWith() {
 		return startingClass;
 	}
 	
 	public int getDefaultHonor() {
 		return defaultHonor;
+	}
+	
+	public int id() {
+		return ordinal();
+	}
+	
+	public static ShogunSocialClass getClassByID(int id) {
+		return classByID.get(id);
 	}
 	
 }
