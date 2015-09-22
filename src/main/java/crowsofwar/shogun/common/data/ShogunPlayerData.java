414046cd013a43c04c9f0cec265795f9a6727680
Crows of War <ofwarcrows@gmail.com>
2015-09-22 16:55:55 -0500

    Player data stores the NPC currently talking to

--- src/main/java/crowsofwar/shogun/common/data/ShogunPlayerData.java
+++ src/main/java/crowsofwar/shogun/common/data/ShogunPlayerData.java
@@ -5,12 +5,14 @@ import java.util.List;
 import java.util.UUID;
 
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.world.World;
 
 import crowsofwar.gorecore.data.GoreCoreDataSaver;
 import crowsofwar.gorecore.data.GoreCorePlayerData;
 import crowsofwar.gorecore.util.GoreCoreNBTInterfaces.CreateFromNBT;
 import crowsofwar.gorecore.util.GoreCoreNBTInterfaces.WriteToNBT;
 import crowsofwar.gorecore.util.GoreCoreNBTUtil;
+import crowsofwar.shogun.common.entity.ShogunNPC;
 import crowsofwar.shogun.common.society.ShogunAction;
 import crowsofwar.shogun.common.society.ShogunSocialClass;
 
@@ -19,10 +21,16 @@ public class ShogunPlayerData extends GoreCorePlayerData {
 	private ShogunSocialClass socialClass;
 	private List<ShogunAction> actionLog;
 	
+	/**
+	 * The ID of the NPC this player is currently talking to. -1 if not talking. This isn't saved because the opened GUI isn't saved.
+	 */
+	private long talkingTo;
+	
 	public ShogunPlayerData(GoreCoreDataSaver dataSaver, UUID playerID) {
 		super(dataSaver, playerID);
 		
 		actionLog = new ArrayList<ShogunAction>();
+		talkingTo = -1;
 		
 	}
 	
@@ -41,6 +49,26 @@ public class ShogunPlayerData extends GoreCorePlayerData {
 	
 	public void setActionLog(List<ShogunAction> actionLog) {
 		this.actionLog = actionLog;
+	}
+	
+	public long getTalkingTo() {
+		return talkingTo;
+	}
+	
+	public ShogunNPC getTalkingToNPC(World world) {
+		// TODO Find a better approach. Use maps?
+		for (Object obj : world.loadedEntityList) {
+			if (obj instanceof ShogunNPC && ((ShogunNPC) obj).getShogunID() == talkingTo) return (ShogunNPC) obj;
+		}
+		return null;
+	}
+	
+	public void setTalkingTo(long talkingTo) {
+		this.talkingTo = talkingTo;
+	}
+	
+	public void setTalkingToNPC(ShogunNPC npc) {
+		setTalkingTo(npc.getShogunID());
 	}
 	
 	@Override
