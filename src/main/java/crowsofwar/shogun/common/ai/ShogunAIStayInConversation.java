af33e18994a74e26f790d0c991bb4bcc316ddd07
Crows of War <ofwarcrows@gmail.com>
2015-09-22 18:55:35 -0500

    Added a "stay in conversation" AI module

---
+++ src/main/java/crowsofwar/shogun/common/ai/ShogunAIStayInConversation.java
@@ -0,0 +1,36 @@
+package crowsofwar.shogun.common.ai;
+
+import crowsofwar.shogun.common.entity.ShogunNPC;
+import net.minecraft.entity.ai.EntityAIBase;
+import net.minecraft.entity.player.EntityPlayer;
+
+/**
+ * AI to walk towards the player that the NPC is having the conversation
+ * with
+ * 
+ * @author CrowsOfWar
+ */
+public class ShogunAIStayInConversation extends EntityAIBase {
+	
+	private final ShogunNPC npc;
+	
+	public ShogunAIStayInConversation(ShogunNPC npc) {
+		this.npc = npc;
+	}
+	
+	@Override
+	public boolean continueExecuting() {
+		if (shouldExecute()) {
+			npc.getNavigator().tryMoveToEntityLiving(npc.getPlayerTalkingTo(), 1);
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	@Override
+	public boolean shouldExecute() {
+		return npc.getPlayerTalkingTo() != null;
+	}
+	
+}
