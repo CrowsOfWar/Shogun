aa04995aaf8c5ce471e41173d48401bcf03fdf45
Crows of War <ofwarcrows@gmail.com>
2015-09-22 16:58:10 -0500

    Created gui handler

---
+++ src/main/java/crowsofwar/shogun/common/gui/ShogunGuiHandler.java
@@ -0,0 +1,37 @@
+package crowsofwar.shogun.common.gui;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.world.World;
+import cpw.mods.fml.common.FMLLog;
+import cpw.mods.fml.common.network.IGuiHandler;
+import crowsofwar.shogun.Shogun;
+import crowsofwar.shogun.common.data.ShogunPlayerData;
+import crowsofwar.shogun.common.data.ShogunPlayerDataFetcher;
+import static crowsofwar.shogun.common.management.ShogunGuiIDs.*;
+
+/**
+ * The GUI handler for the Shogun mod - keeps track of GUI.
+ * 
+ * @author CrowsOfWar
+ */
+public class ShogunGuiHandler implements IGuiHandler {
+	
+	@Override
+	public Object getServerGuiElement(int id, EntityPlayer player, World world, int x, int y, int z) {
+		ShogunPlayerData data = ShogunPlayerDataFetcher.FETCHER.getDataPerformance(player);
+		switch (id) {
+			case ID_CONVERSATION: {
+				return new ShogunContainerConversation(player, data.getTalkingToNPC(world));
+			}
+		}
+		
+		FMLLog.bigWarning("Shogun> This is a bug! Problem with making a new server-side GUI because the GUI ID was incorrect: " + id);
+		return null;
+	}
+	
+	@Override
+	public Object getClientGuiElement(int id, EntityPlayer player, World world, int x, int y, int z) {
+		return Shogun.proxy.createClientGUI(id, player, world, x, y, z);
+	}
+	
+}
