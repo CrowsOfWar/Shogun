3ec81488790f9ccee3b52a0d100f7b473ee06f5a
Crows of War <ofwarcrows@gmail.com>
2015-09-22 18:42:42 -0500

    Added the createClientGUI method

--- src/main/java/crowsofwar/shogun/client/ShogunClientProxy.java
+++ src/main/java/crowsofwar/shogun/client/ShogunClientProxy.java
@@ -1,10 +1,18 @@
 package crowsofwar.shogun.client;
 
+import static crowsofwar.shogun.common.management.ShogunGuiIDs.ID_CONVERSATION;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.world.World;
 import cpw.mods.fml.client.registry.RenderingRegistry;
+import cpw.mods.fml.common.FMLLog;
+import crowsofwar.shogun.client.gui.ShogunGUIConversation;
 import crowsofwar.shogun.client.render.ShogunRenderNPC;
 import crowsofwar.shogun.common.ShogunCommonProxy;
+import crowsofwar.shogun.common.data.ShogunPlayerData;
+import crowsofwar.shogun.common.data.ShogunPlayerDataFetcher;
 import crowsofwar.shogun.common.entity.ShogunNPCPeasant;
 import crowsofwar.shogun.common.entity.ShogunNPCTestBrain;
+import crowsofwar.shogun.common.gui.ShogunContainerConversation;
 
 public class ShogunClientProxy extends ShogunCommonProxy {
 
@@ -12,6 +20,19 @@ public class ShogunClientProxy extends ShogunCommonProxy {
 	public void sideSpecifics() {
 		RenderingRegistry.registerEntityRenderingHandler(ShogunNPCPeasant.class, new ShogunRenderNPC("peasant", 2));
 		RenderingRegistry.registerEntityRenderingHandler(ShogunNPCTestBrain.class, new ShogunRenderNPC("peasant", 1));
+	}
+	
+	@Override
+	public Object createClientGUI(int id, EntityPlayer player, World world, int x, int y, int z) {
+		ShogunPlayerData data = ShogunPlayerDataFetcher.FETCHER.getDataPerformance(player);
+		switch (id) {
+			case ID_CONVERSATION: {
+				return new ShogunGUIConversation(new ShogunContainerConversation(player, data.getTalkingToNPC(world)));
+			}
+		}
+		
+		FMLLog.bigWarning("Shogun> This is a bug! Problem with making a new server-side GUI because the GUI ID was incorrect: " + id);
+		return null;
 	}
 	
 }
--- src/main/java/crowsofwar/shogun/common/ShogunCommonProxy.java
+++ src/main/java/crowsofwar/shogun/common/ShogunCommonProxy.java
@@ -1,7 +1,17 @@
 package crowsofwar.shogun.common;
 
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.world.World;
+
 public class ShogunCommonProxy {
 	
 	public void sideSpecifics() {}
+	
+	/**
+	 * Make a client-side GUI element
+	 */
+	public Object createClientGUI(int id, EntityPlayer player, World world, int x, int y, int z) {
+		return null;
+	}
 	
 }
