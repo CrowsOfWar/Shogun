9f55e72dbfa22293e143995b2babcf160221131c
Crows of War <ofwarcrows@gmail.com>
2015-09-22 16:38:22 -0500

    Added conversation Container

---
+++ src/main/java/crowsofwar/shogun/common/gui/ShogunContainerConversation.java
@@ -0,0 +1,34 @@
+package crowsofwar.shogun.common.gui;
+
+import crowsofwar.shogun.common.entity.ShogunNPC;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.Container;
+
+/**
+ * The container for the conversation GUI.
+ * 
+ * @author CrowsOfWar
+ */
+public class ShogunContainerConversation extends Container {
+	
+	/**
+	 * The player talking to the NPC
+	 */
+	private final EntityPlayer player;
+	
+	/**
+	 * The NPC talking to the player
+	 */
+	private final ShogunNPC npc;
+	
+	public ShogunContainerConversation(EntityPlayer player, ShogunNPC npc) {
+		this.player = player;
+		this.npc = npc;
+	}
+	
+	@Override
+	public boolean canInteractWith(EntityPlayer player) {
+		return player == this.player && player.getDistanceSqToEntity(npc) <= 5*5; // TODO Make the range configurable and stuff
+	}
+	
+}
