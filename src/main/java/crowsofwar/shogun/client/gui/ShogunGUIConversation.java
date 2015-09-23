c17ee1cd608349d5f44d90a43fd02a9f92b43652
Crows of War <ofwarcrows@gmail.com>
2015-09-23 09:56:37 -0500

    Localization of buttons/prompts

--- src/main/java/crowsofwar/shogun/client/gui/ShogunGUIConversation.java
+++ src/main/java/crowsofwar/shogun/client/gui/ShogunGUIConversation.java
@@ -6,6 +6,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.inventory.Container;
+import net.minecraft.util.StatCollector;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import crowsofwar.shogun.Shogun;
@@ -13,7 +14,6 @@ import crowsofwar.shogun.common.conversations.ShogunConversation;
 import crowsofwar.shogun.common.conversations.ShogunConversationChangeReciever;
 import crowsofwar.shogun.common.conversations.ShogunResponse;
 import crowsofwar.shogun.common.entity.ShogunNPC;
-import crowsofwar.shogun.common.gui.ShogunContainerConversation;
 import crowsofwar.shogun.common.packet.ShogunPacketC2SConversationRespond;
 
 @SideOnly(Side.CLIENT)
@@ -38,29 +38,18 @@ public class ShogunGUIConversation extends GuiContainer implements ShogunConvers
 	
 	@Override
 	protected void drawGuiContainerBackgroundLayer(float partialTicks, int mouseX, int mouseY) {
-		fontRendererObj.drawString("Prompt: " + getConversation().getCurrentPrompt(), 10, 10, 0x000000, false);
-//		System.out.println(System.identityHashCode(buttonList));
-//		System.out.println(needToAddButtons + ", " + buttonList); // WHY THE HECK IS BUTTON LIST CLEARING!?!
-		// It's not changing the actual instance... identity hashcode is the same
-//		fontRendererObj.drawString(EnumChatFormatting.UNDERLINE + "Responses", 10, 30, 0x000000, false);
-//		List<ShogunResponse> responses = getConversation().getCurrentResponses();
-//		for (ShogunResponse resp : responses) {
-//			fontRendererObj.drawString(" - " + getConversation().getCurrentPrompt(), 10, 10, 0x000000, false);
-//		}
-	}
+//		fontRendererObj.drawString("Prompt: " + getConversation().getCurrentPrompt(), 10, 10, 0x000000, false);
+		drawCenteredString(fontRendererObj, StatCollector.translateToLocalFormatted("shogun.prompt." + getConversation().getCurrentPrompt().getLanguageKey()), width / 2, 10, 0xffffff);
+	}//GuiCrafting
 
 	@Override
 	public void onChanged() {
-		System.out.println("onChanged()");
 		needToAddButtons = true;
 	}
 	
 	@Override
 	public void updateScreen() {
 		super.updateScreen();
-//		System.out.println("Converation " + getConversation());
-//		System.out.println("need to add buttons? " + needToAddButtons);
-//		System.out.println("Responses: " + getConversation().getCurrentResponses());
 		if (needToAddButtons && getConversation().getCurrentResponses() != null) {
 			addButtons();
 			needToAddButtons = false;
@@ -72,8 +61,8 @@ public class ShogunGUIConversation extends GuiContainer implements ShogunConvers
 		buttonList.clear();
 		List<ShogunResponse> responses = getConversation().getCurrentResponses();
 		for (int i = 0; i < responses.size(); i++) {
-			System.out.println("Respond " + i + ": " + responses.get(i));
-			buttonList.add(new GuiButton(i, 10, (i + 1) * 30, 400, 20, "Respond: " + responses.get(i)));
+			buttonList.add(new GuiButton(i, 10, (i + 1) * 30, 400, 20,
+					StatCollector.translateToLocalFormatted("shogun.response." + responses.get(i).getLanguageKey())));
 		}
 		System.out.println("button list is now:" + buttonList);
 //		System.out.println(System.identityHashCode(buttonList));
