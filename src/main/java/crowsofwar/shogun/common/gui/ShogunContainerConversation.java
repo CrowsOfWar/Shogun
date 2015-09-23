085091889f032f3a7bfe5cb4576491915f6a9bb8
Crows of War <ofwarcrows@gmail.com>
2015-09-22 19:05:58 -0500

    When the conversation has ended, the NPC and player data are reset

--- src/main/java/crowsofwar/shogun/common/gui/ShogunContainerConversation.java
+++ src/main/java/crowsofwar/shogun/common/gui/ShogunContainerConversation.java
@@ -1,5 +1,6 @@
 package crowsofwar.shogun.common.gui;
 
+import crowsofwar.shogun.common.data.ShogunPlayerDataFetcher;
 import crowsofwar.shogun.common.entity.ShogunNPC;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.Container;
@@ -29,6 +30,13 @@ public class ShogunContainerConversation extends Container {
 	@Override
 	public boolean canInteractWith(EntityPlayer player) {
 		return player == this.player && player.getDistanceSqToEntity(npc) <= 5*5; // TODO Make the range configurable and stuff
+	}
+	
+	@Override
+	public void onContainerClosed(EntityPlayer player) {
+		super.onContainerClosed(player);
+		npc.talkTo(null);
+		ShogunPlayerDataFetcher.FETCHER.getDataPerformance(player).setTalkingTo(-1);
 	}
 	
 }
