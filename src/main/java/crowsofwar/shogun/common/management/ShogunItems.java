3b2cd33d50490018415f7a55687090e515f9d39e
Crows of War <ofwarcrows@gmail.com>
2015-09-29 20:32:54 -0500

    Register oyori set

--- src/main/java/crowsofwar/shogun/common/management/ShogunItems.java
+++ src/main/java/crowsofwar/shogun/common/management/ShogunItems.java
@@ -1,12 +1,18 @@
 package crowsofwar.shogun.common.management;
 
 import cpw.mods.fml.common.registry.GameRegistry;
+import crowsofwar.shogun.common.item.ShogunArmorSet;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemSlab;
+import net.minecraft.item.ItemArmor.ArmorMaterial;
 import static crowsofwar.shogun.common.management.ShogunBlocks.*;
 
 public class ShogunItems {
 	
+	public static ShogunArmorSet armorSetOyori;
+	
 	public static void initAndRegister() {
+		armorSetOyori = new ShogunArmorSet(ArmorMaterial.IRON, ShogunCreativeTabs.tabCombat, "o-yori");
 	}
+	
 }
